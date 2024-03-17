340dbb06db927b6d7b8a9ce422ded46bf781e4c3
Irham Baehaqi <irhambaebaehaqi@gmail.com>
2024-03-17 23:57:25 +0700

    Menambahkan class Data Generic

--- tpmodul05/tpmodul05/Program.cs
+++ tpmodul05/tpmodul05/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine("Halo user " + nama);
+    }
+}
+
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Masukkan nama pengguna:");
+        string namaPengguna = Console.ReadLine();
+
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(namaPengguna);
+        Console.WriteLine();
+
+        Console.WriteLine("Masukkan data:");
+        string inputData = Console.ReadLine();
+
+        DataGeneric<string> dataGeneric = new DataGeneric<string>(inputData);
+        dataGeneric.PrintData();
+    }
+}
